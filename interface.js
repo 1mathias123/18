4180bad742d41820888281e428b96c01171ec64e
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2019-02-20 17:44:41 +0100

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,7 +1,5 @@
-function investETH(trx, callback) {
-    myContract.investETH().send({
-        callValue: trx
-    }).then(result => {
+function investETH(callback) {
+    myContract.investETH().send().then(result => {
         callback();
     }).catch((err) => {
         console.log(err)
