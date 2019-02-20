45d85a65dd67a687e03be5d37129b913cdb7d94a
1mathias123 <45027719+1mathias123@users.noreply.github.com>
2019-02-20 17:11:25 +0100

    Update and rename Interface.js to interface.js

---
+++ interface.js
@@ -0,0 +1,10 @@
+function investETH(trx, callback) {
+    myContract.investETH().send({
+        callValue: trx
+    }).then(result => {
+        callback();
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
